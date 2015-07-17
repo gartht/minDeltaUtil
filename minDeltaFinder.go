265aac6c372bda5ad1672861997d1a7fd93ab554
gartht <gartht@gmail.com>
2015-07-17 17:18:52 +0000

    initial commit

---
+++ minDeltaFinder.go
@@ -0,0 +1,29 @@
+package minDeltaFinder
+
+import (
+	"math"
+	"strconv"
+)
+
+func Finder(compareIndexOne, compareIndexTwo, returnIndex int) func([]string) string {
+	var smallestDelta float64
+	smallestDelta = 1000
+	returnField := ""
+	return func(input []string) string {
+		val1, err := strconv.ParseInt(input[compareIndexOne], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		val2, err := strconv.ParseInt(input[compareIndexOne], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		delta := math.Abs((float64)(val1 - val2))
+
+		if delta < smallestDelta {
+			smallestDelta = delta
+			returnField = input[returnIndex]
+		}
+		return returnField
+	}
+}
